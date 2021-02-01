ad3a5dfd2c362f788fdc3c364eb3d9cb1bf05336
Yeshwanth Sriram Guntuka <ysriramg@codeaurora.org>
2021-02-01 19:51:31 +0530

    qcacld-3.0: Flush frags for peer on add key request
    Fragments are not flushed as part of rekey which
    could result in fragments encrypted under different
    keys to be reassembled.
    
    Fix is to flush fragments for the peer for which add
    key request is received.
    
    Change-Id: I0c018ff7375272125c62aaea7b8ad4df9e842508
    CRs-Fixed: 2875950

--- core/hdd/src/wlan_hdd_cfg80211.c
+++ core/hdd/src/wlan_hdd_cfg80211.c
@@ -16698,6 +16698,11 @@ static int __wlan_hdd_cfg80211_add_key(struct wiphy *wiphy,
 		set_key.keyDirection = eSIR_TX_RX;
 		qdf_mem_copy(set_key.peerMac.bytes, mac_addr, QDF_MAC_ADDR_SIZE);
 	}
+
+	cdp_peer_flush_frags(cds_get_context(QDF_MODULE_ID_SOC),
+                             cds_get_context(QDF_MODULE_ID_TXRX),
+                             adapter->vdev_id, set_key.peerMac.bytes);
+
 	if ((QDF_IBSS_MODE == adapter->device_mode) && !pairwise) {
 		/* if a key is already installed, block all subsequent ones */
 		if (adapter->session.station.ibss_enc_key_installed) {
