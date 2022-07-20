5eb20fd8d6c847eb5eb7aefb72127af3017ee415
vantoman <Mustafa.vantom@gmail.com>
2022-07-20 09:52:55 +0200

    techpack: display: Address ISO C-90 -Wdeclaration-after-statement
    Fixes a declaration after statement code.
    
    ../techpack/display/msm/dsi/dsi_display.c:5328:6: warning: mixing declarations and code is a C99 extension [-Wdeclaration-after-statement]
            int bl_lvl_before_hbm = display->panel->bl_config.bl_level;
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -5026,6 +5026,7 @@ static ssize_t sysfs_hbm_write(struct device *dev,
 {
 	struct dsi_display *display = dev_get_drvdata(dev);
 	int ret, hbm_mode;
+	int bl_lvl_before_hbm = display->panel->bl_config.bl_level;
 
 	if (!display->panel)
 		return -EINVAL;
@@ -5050,7 +5051,6 @@ static ssize_t sysfs_hbm_write(struct device *dev,
 		goto error;
 	}
 
-	int bl_lvl_before_hbm = display->panel->bl_config.bl_level;
 	ret = dsi_panel_apply_hbm_mode(display->panel);
 	if (ret)
 		DSI_ERR("unable to set hbm mode\n");
