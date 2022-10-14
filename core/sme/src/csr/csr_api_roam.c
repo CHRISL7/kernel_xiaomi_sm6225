a090f4f5b81737a5b5e843d8e22f5c5e4c1e92f4
Krupali Dhanvijay <quic_kdhanvij@quicinc.com>
2022-10-14 16:42:59 +0530

    qcacld-3.0: Update pmk for roamed AP to pmk cache table
    Assume AP1 and AP2 are SPMK APs. For SPMK AP(s), Host
    should add an entry of an AP in PMK cache table like below in
    two cases only:
    Case 1. When DUT successfully associated with SPMK supported AP
                In this case host update “is_spmk_ap” flag in PMK
                table by parsing beacon of associated AP after
                successful connection.
    Case 2. When DUT successfully roamed to SPMK supported AP
                In this case host update “is_spmk_ap” flag in PMK
                table by parsing roam sync indication event.
    
    In case of connection with SPMK AP, Host selectively deletes PMK
    entry for other SPMK supported AP(s) on basis of “is_spmk_ap”
    flag and maintains only one entry for all SPMK AP(s). And host
    sends the same single PMK in RSO for further roaming to SPMK AP.
    Initially, DUT is connected with AP2. Then Disconnection happens with
    AP2 due to NUD failure. After disconnection, the upper layer sends
    flush PMK requests for AP1 and AP2. Host deletes old PMK entries for
    both APs. Now upper layer sends a set PMK request for AP2. Host adds
    AP2 entry in PMK cache table but host does not set "is_spmk_ap" flag
    in PMK table for this entry as DUT is not connected to AP2. Now host
    receives a connect request for AP1 from the upper layer. DUT
    successfully associated with AP1 by performing full SAE authentication.
    Host adds an entry for AP1 in the PMK cache table and sets "is_spmk_ap"
    flag for AP1 but fails to delete the entry for other SPMK AP(s), here
    AP2, from PMK cache table. This is because of "is_spmk_ap" flag is not
    set for AP2. At this point of time below is the PMK cache table entry
    for SPMK AP(s): The Host PMK cache table has two entries for two SPMK
    APs.
    
       BSSID        PMK     is_spmk_ap flag
        AP2        PMK2           0
        AP1        PMK1           1
    
    Now FW roams to AP2 using PMK1. Host process roam sync indication for
    AP2 and updates "is_spmk_ap" flag for AP2 in the PMK cache table. As
    Host has a stale entry for AP2 in the PMK cache table, Host sends AP2’s
    PMK (here PMK2) in RSO command which firmware will use for further
    roaming but roaming fails due to invalid PMK, as target SPMK AP expects
    PMK1 in reassociation request.
    To handle these scenarios, FW should send PMK info of roamed AP and
    host override stale entry for roamed AP (if any) with roamed AP's PMK
    in PMK cache table.
    
    Change-Id: I68188d447d2decbe2a1cdcf5f78d11df0b5276f9
    CRs-Fixed: 3294391

--- core/sme/src/csr/csr_api_roam.c
+++ core/sme/src/csr/csr_api_roam.c
@@ -15529,6 +15529,30 @@ csr_store_sae_single_pmk_to_global_cache(struct mac_context *mac,
 	qdf_mem_zero(pmk_info, sizeof(*pmk_info));
 	qdf_mem_free(pmk_info);
 }
+
+void
+csr_set_sae_single_pmk_info(struct csr_roam_session *session,
+			    tPmkidCacheInfo *roam_sync_pmksa)
+{
+	tPmkidCacheInfo *cached_pmksa;
+	int i;
+
+	for (i = 0; i < session->NumPmkidCache; i++) {
+		cached_pmksa = &session->PmkidCacheInfo[i];
+		if (qdf_is_macaddr_equal(&roam_sync_pmksa->BSSID,
+					 &cached_pmksa->BSSID) &&
+		    roam_sync_pmksa->single_pmk_supported &&
+		    roam_sync_pmksa->pmk_len) {
+			cached_pmksa->single_pmk_supported =
+					roam_sync_pmksa->single_pmk_supported;
+			cached_pmksa->pmk_len =
+					roam_sync_pmksa->pmk_len;
+			qdf_mem_copy(cached_pmksa->pmk,
+				     roam_sync_pmksa->pmk,
+				     roam_sync_pmksa->pmk_len);
+		}
+	}
+}
 #endif
 
 void csr_update_pmk_cache_ft(struct mac_context *mac,
@@ -23072,11 +23096,12 @@ static bool csr_is_sae_single_pmk_vsie_ap(struct bss_description *bss_des)
 static void
 csr_check_and_set_sae_single_pmk_cap(struct mac_context *mac_ctx,
 				     struct csr_roam_session *session,
-				     uint8_t vdev_id)
+				     uint8_t vdev_id, uint8_t *psk_pmk,
+				     uint8_t pmk_len)
 {
 	struct wlan_objmgr_vdev *vdev;
 	struct mlme_pmk_info *pmk_info;
-	tPmkidCacheInfo *pmkid_cache;
+	tPmkidCacheInfo *pmkid_cache, *roam_sync_pmksa;
 	uint32_t pmkid_index;
 	bool val, lookup_success;
 
@@ -23094,9 +23119,23 @@ csr_check_and_set_sae_single_pmk_cap(struct mac_context *mac_ctx,
 		if (!val)
 			goto end;
 
-		csr_set_sae_single_pmk_bss_cap(session, true,
-					       (struct qdf_mac_addr *)
-					       session->pConnectBssDesc->bssId);
+		roam_sync_pmksa = qdf_mem_malloc(sizeof(*roam_sync_pmksa));
+		if (roam_sync_pmksa) {
+			qdf_copy_macaddr(&roam_sync_pmksa->BSSID,
+					 (struct qdf_mac_addr *)
+					 &session->pConnectBssDesc->bssId);
+			roam_sync_pmksa->single_pmk_supported = true;
+			roam_sync_pmksa->pmk_len = pmk_len;
+			qdf_mem_copy(roam_sync_pmksa->pmk, psk_pmk,
+				     roam_sync_pmksa->pmk_len);
+			/* update single pmk info for roamed ap to pmk table */
+			csr_set_sae_single_pmk_info(session,
+						    roam_sync_pmksa);
+			qdf_mem_zero(roam_sync_pmksa, sizeof(*roam_sync_pmksa));
+			qdf_mem_free(roam_sync_pmksa);
+		} else {
+			goto end;
+		}
 
 		pmkid_cache = qdf_mem_malloc(sizeof(*pmkid_cache));
 		if (!pmkid_cache)
@@ -23145,7 +23184,8 @@ end:
 static inline void
 csr_check_and_set_sae_single_pmk_cap(struct mac_context *mac_ctx,
 				     struct csr_roam_session *session,
-				     uint8_t vdev_id)
+				     uint8_t vdev_id, uint8_t *psk_pmk,
+				     uint8_t pmk_len)
 {
 }
 #endif
@@ -23300,8 +23340,14 @@ static QDF_STATUS csr_process_roam_sync_callback(struct mac_context *mac_ctx,
 
 		if (ucfg_pkt_capture_get_pktcap_mode())
 			ucfg_pkt_capture_record_channel(vdev);
-		csr_check_and_set_sae_single_pmk_cap(mac_ctx, session,
-						     session_id);
+
+		if (roam_synch_data->pmk_len) {
+			csr_check_and_set_sae_single_pmk_cap(
+						mac_ctx, session,
+						session_id,
+						roam_synch_data->pmk,
+						roam_synch_data->pmk_len);
+		}
 
 		if (WLAN_REG_IS_5GHZ_CH(bss_desc->channelId)) {
 			session->disable_hi_rssi = true;
