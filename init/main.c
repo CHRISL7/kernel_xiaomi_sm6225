983b56098003c714d26e9af04db5db0b606f85a1
Cyber Knight <cyberknight755@gmail.com>
2023-03-01 23:57:45 +0800

    init: Add cmdline interface to check if dt2w sensor is used
    - This flag will be provided by our bootargs from device tree to facilitate support for legacy dt2w implementations that are supposed to be disabled when the dt2w sensor is used.
    
    Change-Id: I580b0e83661889cd00a953cb3f2f0e2606290966
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- init/main.c
+++ init/main.c
@@ -105,6 +105,16 @@ static int kernel_init(void *);
 extern void init_IRQ(void);
 extern void radix_tree_init(void);
 
+bool is_dt2w_sensor = false;
+EXPORT_SYMBOL(is_dt2w_sensor);
+
+static int __init read_is_dt2w_sensor(char *s)
+{
+    strtobool(s, &is_dt2w_sensor);
+    return 1;
+}
+__setup("init.is_dt2w_sensor=", read_is_dt2w_sensor);
+
 /*
  * Debug helper: via this flag we know that we are in 'early bootup code'
  * where only the boot processor is running with IRQ disabled.  This means
