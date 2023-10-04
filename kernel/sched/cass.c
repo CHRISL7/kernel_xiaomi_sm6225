e1f13d3d356833a29fadcc0d10a4dfbf5c1d7ffb
EmanuelCN <emanuelghub@gmail.com>
2023-10-04 11:36:02 +0300

    kernel: sched/cass: Adapt for msm-4.19
    Below patches above cass are required too.

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -40,7 +40,7 @@ unsigned long cass_cpu_util(int cpu, bool sync)
 
 	/* Deduct @current's util from this CPU if this is a sync wake */
 	if (sync && cpu == smp_processor_id())
-		lsub_positive(&util, task_util(current));
+		sub_positive(&util, task_util(current));
 
 	if (sched_feat(UTIL_EST))
 		util = max_t(unsigned long, util,
@@ -101,7 +101,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 	int cidx = 0, cpu;
 
 	/* Get the utilization for this task */
-	p_util = clamp(task_util_est(p),
+	p_util = clamp(task_util(p),
 		       uclamp_eff_value(p, UCLAMP_MIN),
 		       uclamp_eff_value(p, UCLAMP_MAX));
 
@@ -111,7 +111,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 	 * preemptible and RCU-sched is unified with normal RCU. Therefore,
 	 * non-preemptible contexts are implicitly RCU-safe.
 	 */
-	for_each_cpu_and(cpu, p->cpus_ptr, cpu_active_mask) {
+	for_each_cpu_and(cpu, &p->cpus_allowed, cpu_active_mask) {
 		/* Use the free candidate slot */
 		curr = &cands[cidx];
 		curr->cpu = cpu;
@@ -120,8 +120,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 		 * Check if this CPU is idle or only has SCHED_IDLE tasks. For
 		 * sync wakes, always treat the current CPU as idle.
 		 */
-		if ((sync && cpu == smp_processor_id()) ||
-		    available_idle_cpu(cpu) || sched_idle_cpu(cpu)) {
+		if ((sync && cpu == smp_processor_id()) || available_idle_cpu(cpu)) {
 			/* Discard any previous non-idle candidate */
 			if (!has_idle) {
 				best = curr;
@@ -180,7 +179,8 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 }
 
 static int cass_select_task_rq_fair(struct task_struct *p, int prev_cpu,
-				    int sd_flag, int wake_flags)
+				    int sd_flag, int wake_flags,
+				    int sibling_count_hint)
 {
 	bool sync;
 
@@ -193,8 +193,8 @@ static int cass_select_task_rq_fair(struct task_struct *p, int prev_cpu,
 	 * first valid CPU since it's possible for certain types of tasks to run
 	 * on inactive CPUs.
 	 */
-	if (unlikely(!cpumask_intersects(p->cpus_ptr, cpu_active_mask)))
-		return cpumask_first(p->cpus_ptr);
+	if (unlikely(!cpumask_intersects(&p->cpus_allowed, cpu_active_mask)))
+		return cpumask_first(&p->cpus_allowed);
 
 	/* cass_best_cpu() needs the task's utilization, so sync it up */
 	if (!(sd_flag & SD_BALANCE_FORK))
