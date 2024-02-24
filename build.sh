b0d95a3468045908d6339a6afbfd1e46ece75beb
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-02-24 22:22:35 +0000

    build.sh: Add device int the ZIPNAME
    Change-Id: I19ee22db8849d04a4145344085e32080e048f50b
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # Copyright (C) 2020-2021 Adithya R.
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="uvite-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME="uvite-$(date '+%Y%m%d-%H%M')-spes.zip"
 TC_DIR="$(pwd)/tc/clang-r450784e"
 AK3_DIR="$(pwd)/android/AnyKernel3"
 DEFCONFIG="vendor/spes-perf_defconfig"
