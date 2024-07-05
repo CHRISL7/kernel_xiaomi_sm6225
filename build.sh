54f8f84e17f734f3a9b3140188ef95cd42f11d37
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-07-05 00:57:02 +0000

    build: Use Full LLVM
    [1] - https://github.com/AOSPA/android_device_xiaomi_spes/commit/85924162525dc7a398facd59f5277ba1e28910a2
    
    Change-Id: I4a0853576c46be6f9e6961ef64e9c7b5b1f64af5
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 Image.gz dtbo.img 2> >(tee log.txt >&2) || exit $?
+make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 Image.gz dtbo.img 2> >(tee log.txt >&2) || exit $?
 
 kernel="out/arch/arm64/boot/Image.gz"
 dtbo="out/arch/arm64/boot/dtbo.img"
