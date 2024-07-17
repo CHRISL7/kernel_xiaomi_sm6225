5d2e95e5bb452359fc43499ea0f3d01aea6c18eb
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-07-17 21:18:16 +0000

    drivers: thermal: Use drm panel
    Change-Id: Ic9583e42bbcb19994e887c7a2019a77edd6b649c
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -24,7 +24,7 @@
 #include <linux/suspend.h>
 #include <linux/cpu_cooling.h>
 #ifdef CONFIG_DRM
-#include <drm/drm_notifier_mi.h>
+#include <drm/drm_panel.h>
 #endif
 
 #define CREATE_TRACE_POINTS
@@ -1943,13 +1943,13 @@ static void destroy_thermal_message_node(void)
 static const char *get_screen_state_name(int mode)
 {
 	switch (mode) {
-	case MI_DRM_BLANK_UNBLANK:
+	case DRM_BLANK_UNBLANK:
 		return "On";
-	case MI_DRM_BLANK_LP1:
+	case DRM_BLANK_LP1:
 		return "Doze";
-	case MI_DRM_BLANK_LP2:
+	case DRM_BLANK_LP2:
 		return "DozeSuspend";
-	case MI_DRM_BLANK_POWERDOWN:
+	case DRM_BLANK_POWERDOWN:
 		return "Off";
 	default:
 		return "Unknown";
@@ -1959,20 +1959,20 @@ static const char *get_screen_state_name(int mode)
 static int screen_state_for_thermal_callback(struct notifier_block *nb,
 		unsigned long val, void *data)
 {
-	struct mi_drm_notifier *evdata = data;
+	struct drm_notify_data *evdata = data;
 	unsigned int blank;
 
-	if (val != MI_DRM_EVENT_BLANK || !evdata || !evdata->data)
+	if (val != DRM_EVENT_BLANK || !evdata || !evdata->data)
 		return 0;
 
 	blank = *(int *)(evdata->data);
 	switch (blank) {
-	case MI_DRM_BLANK_UNBLANK:
+	case DRM_BLANK_UNBLANK:
 		sm.screen_state = 1;
 		break;
-	case MI_DRM_BLANK_LP1:
-	case MI_DRM_BLANK_LP2:
-	case MI_DRM_BLANK_POWERDOWN:
+	case DRM_BLANK_LP1:
+	case DRM_BLANK_LP2:
+	case DRM_BLANK_POWERDOWN:
 		sm.screen_state = 0;
 		break;
 	default:
@@ -2030,7 +2030,7 @@ static int __init thermal_init(void)
 
 #ifdef CONFIG_DRM
 	sm.thermal_notifier.notifier_call = screen_state_for_thermal_callback;
-	if (mi_drm_register_client(&sm.thermal_notifier) < 0) {
+	if (drm_register_client(&sm.thermal_notifier) < 0) {
 		pr_warn("Thermal: register screen state callback failed\n");
 	}
 #endif
@@ -2055,7 +2055,7 @@ error:
 static void thermal_exit(void)
 {
 #ifdef CONFIG_DRM
-	mi_drm_unregister_client(&sm.thermal_notifier);
+	drm_unregister_client(&sm.thermal_notifier);
 #endif
 	unregister_pm_notifier(&thermal_pm_nb);
 	of_thermal_destroy_zones();
